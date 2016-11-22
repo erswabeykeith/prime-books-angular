ff49a87083bd394aa35466c3bddf455778336a81
Liz Swabey-Keith <lizswabey-keith@Lizs-MacBook-Pro-2.local>
2016-11-22 13:47:31 -0600

    hard mode done with filtering

--- server/public/scripts/client-app.js
+++ server/public/scripts/client-app.js
@@ -6,7 +6,6 @@ myApp.controller("BookController", ["$http", function($http) {
   var self = this;
   self.newBook = {};
   self.books = [];
-  self.updatedBook = {},
 
   getBooks();
 
@@ -51,6 +50,20 @@ myApp.controller("BookController", ["$http", function($http) {
 
 
 }]);
+
+myApp.filter('unique', function() {
+
+ return function (arr, field) {
+   var o = {}, i, l = arr.length, r = [];
+   for(i=0; i<l;i+=1) {
+     o[arr[i][field]] = arr[i];
+   }
+   for(i in o) {
+     r.push(o[i]);
+   }
+   return r;
+ };
+})
 
 
 
